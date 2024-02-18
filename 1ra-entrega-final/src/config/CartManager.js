024000268b58838a06451e60a27683ca1cb0a542
FedeDalDegan <federiconahueldaldegan@hotmail.com>
2024-02-18 19:19:42 -0300

    Cambios en addProductToCart

--- 1ra-entrega-final/src/config/CartManager.js
+++ 1ra-entrega-final/src/config/CartManager.js
@@ -21,13 +21,21 @@ export class CartManager {
 
     addProductToCart = async(idProducto, quantity) => {
         const cart = JSON.parse(await fs.readFile(this.products, "utf-8"))
-
-        const indice = cart.findIndex(product => product.id === idProducto) // Que traiga un producto cuyo ID sea igual al ID que llega como parametro
-        if(indice != -1){ // Si da DISTINTO de -1. Existe
-            cart[indice].quantity += quantity // En la posicion del indice de cart, le modifico la cantidad en caso de que el ID sea encontrado.
+        if(cart.length){
+            return "No existe ningun carrito"
+        }
+        const restCarts = cart.filter((c) => c.id !== cartId);
+        const cartSelected = cart.find((c) => c.id === cartId);
+        let product = cartSelected.products.find(
+            (product) => product.id === idProducto
+        )
+        if(product){ 
+            product.quantity += quantity // Cunado el ID sea encontrado, le modifico su cantidad
+            restCarts.push(cartSelected)
         }else{
             const prod = {id: idProducto, quantity: quantity} // En caso de no existir, lo creo con su ID y su CANTIDAD
-            cart.push(prod)
+            cartSelected.products.push(prod)
+            restCarts.push(cartSelected)
         }
         await fs.writeFile(this.products, JSON.stringify(cart))  // Sea cual sea el caso, se vuelve a reescribir el archivo
         return "Producto cargado correctamente"
